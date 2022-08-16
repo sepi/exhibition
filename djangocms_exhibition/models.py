02ef50403512932c36aa5c183f5607534d33a6cf
Raffael Mancini <raffael.mancini@mnhn.lu>
2022-08-16 13:16:15 +0200

    better stuff

--- djangocms_exhibition/models.py
+++ djangocms_exhibition/models.py
@@ -1,12 +1,14 @@
 from cms.models.pluginmodel import CMSPlugin
 from django.db import models
+from djangocms_text_ckeditor.fields import HTMLField
 
 class ExhibitionObject(CMSPlugin):
-    author = models.CharField(max_length=255)
-    date = models.CharField(max_length=128)
-    name_en = models.CharField(max_length=255)
-    description_en = models.TextField()
-    name_fr = models.CharField(max_length=255)
-    description_fr = models.TextField()
-    name_de = models.CharField(max_length=255)
-    description_de = models.TextField()
+    author = models.CharField(max_length=255, null=True, blank=True)
+    date_author = models.CharField(max_length=128, null=True, blank=True)
+
+    info_object = models.TextField(null=True, blank=True)
+    date_object = models.CharField(max_length=128, null=True, blank=True)
+
+    comment_fr = HTMLField(null=True, blank=True)
+    comment_de = HTMLField(null=True, blank=True)
+    comment_en = HTMLField(null=True, blank=True)
