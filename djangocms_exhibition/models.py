68f2cf24374a3e0a1e3c2995d8e097850cca43ef
Raffael Mancini <raffael.mancini@mnhn.lu>
2022-08-23 21:29:08 +0200

    Better layout and more translated fields

--- djangocms_exhibition/models.py
+++ djangocms_exhibition/models.py
@@ -6,9 +6,14 @@ class ExhibitionObject(CMSPlugin):
     author = models.CharField(max_length=255, null=True, blank=True)
     date_author = models.CharField(max_length=128, null=True, blank=True)
 
-    info_object = models.TextField(null=True, blank=True)
-    date_object = models.CharField(max_length=128, null=True, blank=True)
-
+    info_object_fr = models.TextField(null=True, blank=True)
+    date_object_fr = models.CharField(max_length=128, null=True, blank=True)
     comment_fr = HTMLField(null=True, blank=True)
+
+    info_object_de = models.TextField(null=True, blank=True)
+    date_object_de = models.CharField(max_length=128, null=True, blank=True)
     comment_de = HTMLField(null=True, blank=True)
+
+    info_object_en = models.TextField(null=True, blank=True)
+    date_object_en = models.CharField(max_length=128, null=True, blank=True)
     comment_en = HTMLField(null=True, blank=True)
