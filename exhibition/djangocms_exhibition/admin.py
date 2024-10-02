22e5a58e08a3ca864ae63dff3f86a12a9f7e2af1
Raffael Mancini <raffael.mancini@mnhn.lu>
2024-10-02 06:49:42 +0200

    Add exhibition objects to admin

--- exhibition/djangocms_exhibition/admin.py
+++ exhibition/djangocms_exhibition/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import ExhibitionObject
+
 # Register your models here.
+@admin.register(ExhibitionObject)
+class ExhibitionObjectAdmin(admin.ModelAdmin):
+    model = ExhibitionObject
+
+    list_display = ["id", "author", "date_author"]
--- exhibition/djangocms_exhibition/apps.py
+++ exhibition/djangocms_exhibition/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class DjangocmsExhibitionConfig(AppConfig):
     name = 'djangocms_exhibition'
+    verbose_name = 'Exhibition'
