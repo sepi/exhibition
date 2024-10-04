beae99546a03bb382fbf976b2310218e834bebf7
Raffael Mancini <raffael.mancini@mnhn.lu>
2024-10-04 18:15:53 +0200

    Enabled import for Objects

--- exhibition/djangocms_exhibition/admin.py
+++ exhibition/djangocms_exhibition/admin.py
@@ -2,9 +2,11 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 
 from cms.models.titlemodels import Title
+from cms.models.pluginmodel import CMSPlugin
+from cms.constants import PUBLISHER_STATE_DIRTY
 
 from import_export import resources, fields
-from import_export.admin import ExportMixin
+from import_export.admin import ImportMixin, ExportMixin, ImportExportMixin
 
 
 from .models import ExhibitionObject
@@ -23,16 +25,51 @@ class PageFilter(admin.SimpleListFilter):
         else:
             return queryset
 
+def add_to_placeholder(instance, page_slug, is_draft):
+    title = Title.objects.get(slug=page_slug, publisher_is_draft=is_draft)
+    title.publisher_state = PUBLISHER_STATE_DIRTY # So that in edit mode we know this is dirty
+    title.save()
+    
+    placeholder = title.page.placeholders.get(slot='content')
+
+    instance.placeholder = placeholder
+    instance.language = 'en'
+
+    new_pos = CMSPlugin.objects.filter(language='en',
+                                       parent__isnull=True,
+                                       placeholder=placeholder).count()
+    instance.position = new_pos
+    instance.plugin_type = 'ExhibitionObjectPlugin'
+    instance.add_root(instance=instance)
+
+        
 class ExhibitionObjectResource(resources.ModelResource):
+    page_slug = fields.Field()
+
+    def dehydrate_page_slug(self, eo):
+        page = eo.placeholder.page
+        if page:
+            title = Title.objects.get(page=page)
+            return title.slug
+        else:
+            return ''
+
+    def before_save_instance(self, instance, row, **kwargs):
+        page_slug = row.get('page_slug')
+
+        add_to_placeholder(instance, page_slug, True)
+
     class Meta:
         model = ExhibitionObject
-        fields = ('author', 'date_author',
+        fields = ('page_slug', 'author', 'date_author',
                   'info_object_fr', 'date_object_fr', 'comment_fr',
                   'info_object_de', 'date_object_de', 'comment_de',
                   'info_object_en', 'date_object_en', 'comment_en')
 
+        import_id_fields = []
+
 @admin.register(ExhibitionObject)
-class ExhibitionObjectAdmin(ExportMixin, admin.ModelAdmin):
+class ExhibitionObjectAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ["author", "date_author", "page", "image_thumbnail"]
 
     list_filter = (PageFilter, "author", "date_author")
