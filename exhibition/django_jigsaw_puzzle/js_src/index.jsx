c3028d962211f9ad1296445824734bb13d75c6a6
Raffael Mancini <raffael.mancini@mnhn.lu>
2024-12-29 19:46:59 +0100

    Simpler props passing from data-* attrs

--- exhibition/django_jigsaw_puzzle/js_src/index.jsx
+++ exhibition/django_jigsaw_puzzle/js_src/index.jsx
@@ -6,32 +6,9 @@ import './index.css';
 import App from './App';
 
 const rootEl = document.getElementById('jigsaw-puzzle');
-// The API endpoint where to get images from
-const imageUrl = rootEl.dataset.imageUrl;
-// The title shown in navbar
-const title = rootEl.dataset.title;
-// The logo shown in navbar
-const logoUrl = rootEl.dataset.logoUrl;
-// Should image order be random?
-const randomizeImages = (rootEl.dataset.randomizeImages.toLowerCase() == 'true');
-// Show warning after no activity for this amount of seconds
-const idleFirstSeconds = rootEl.dataset.idleFirstSeconds;
-// If warning was shown for this amount of seconds, stop game.
-const idleSecondSeconds = rootEl.dataset.idleSecondSeconds;
-const copyrightNotice = rootEl.dataset.copyrightNotice;
-const navbarBackgroundColor = rootEl.dataset.navbarBackgroundColor;
-
 const root = ReactDOM.createRoot(rootEl);
 root.render(
     <React.StrictMode>
-	<App imageUrl={imageUrl}
-	     title={title}
-	     logoUrl={logoUrl}
-	     randomizeImages={randomizeImages}
-	     idleFirstSeconds={idleFirstSeconds}
-	     idleSecondSeconds={idleSecondSeconds}
-	     copyrightNotice={copyrightNotice}
-	     navbarBackgroundColor={navbarBackgroundColor}
-	/>
+	<App { ...rootEl.dataset } />
     </React.StrictMode>
 );
