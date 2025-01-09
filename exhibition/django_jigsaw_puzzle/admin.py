fe43e13bfe92437efe1be765ae44f793c5927681
Raffael Mancini <raffael.mancini@mnhn.lu>
2025-01-09 23:16:12 +0100

    Refactor Game admins

--- exhibition/django_jigsaw_puzzle/admin.py
+++ exhibition/django_jigsaw_puzzle/admin.py
@@ -27,9 +27,7 @@ class GridDifficultyLevelInline(admin.TabularInline):
     model = GridDifficultyLevel
     extra = 0
 
-
-@admin.register(JigsawPuzzle)
-class JigsawPuzzleAdmin(admin.ModelAdmin):
+class GameAdminMixin():
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         self.request = request
@@ -41,13 +39,6 @@ class JigsawPuzzleAdmin(admin.ModelAdmin):
         GridDifficultyLevelInline
     ]
 
-    fieldsets = (
-        (None, {
-            'fields': ('info_text', 'name', 'copyright_notice', 'color',
-                       'randomize_images', 'image_set'),
-        }),
-    )
-
     # set type="color" to the color field so the color picker is used
     def formfield_for_dbfield(self, db_field, request, **kwargs):
         if db_field.name == 'color':
@@ -56,48 +47,35 @@ class JigsawPuzzleAdmin(admin.ModelAdmin):
 
     def info_text(self, obj):
         if obj.id:
-            link = reverse('jigsaw_puzzle_detail', args=[obj.id])
+            link = self.get_game_link(obj)
             abs_link = self.request.build_absolute_uri(link)
-            return mark_safe(f"<a href=\"{abs_link}\" target=\"_blank\">{abs_link}</a><br/>Use this link only if there is no page in the CMS with a plugin for this puzzle.")
+            return mark_safe(f"<a href=\"{abs_link}\" target=\"_blank\">{abs_link}</a><br/>Use this link only if there is no page in the CMS with a plugin for this game.")
         else:
             return "No link available yet"
 
     info_text.short_description = "Direct link to puzzle"
 
-
-@admin.register(MemoryGame)
-class MemoryGameAdmin(admin.ModelAdmin):
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        self.request = request
-        return qs
-    
-    # readonly_fields = ('info_text',)
-    
-    inlines = [
-        GridDifficultyLevelInline
-    ]
-
+@admin.register(JigsawPuzzle)
+class JigsawPuzzleAdmin(GameAdminMixin, admin.ModelAdmin):
     fieldsets = (
         (None, {
-            'fields': (# 'info_text',
-                       'name', 'copyright_notice', 'color',
+            'fields': ('info_text', 'name', 'copyright_notice', 'color',
+                       'randomize_images', 'image_set'),
+        }),
+    )
+
+    def get_game_link(self, obj):
+        return reverse('jigsaw_puzzle_detail', args=[obj.id])
+
+
+@admin.register(MemoryGame)
+class MemoryGameAdmin(GameAdminMixin, admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('info_text', 'name', 'copyright_notice', 'color',
                        'image_set'),
         }),
     )
 
-    # set type="color" to the color field so the color picker is used
-    def formfield_for_dbfield(self, db_field, request, **kwargs):
-        if db_field.name == 'color':
-            kwargs['widget'] = forms.TextInput(attrs={'type': 'color'})
-        return super().formfield_for_dbfield(db_field, request, **kwargs)
-
-    # def info_text(self, obj):
-    #     if obj.id:
-    #         link = reverse('jigsaw_puzzle_detail', args=[obj.id])
-    #         abs_link = self.request.build_absolute_uri(link)
-    #         return mark_safe(f"<a href='{abs_link}'>{abs_link}</a><br/>Use this link only if there is no page in the CMS with a plugin for this puzzle.")
-    #     else:
-    #         return "No link available yet"
-
-    # info_text.short_description = "Direct link to puzzle"
+    def get_game_link(self, obj):
+        return reverse('memory_game_detail', args=[obj.id])
