50f2962bd80568a4d3eb43e84cbb6a793e276d3d
Raffael Mancini <raffael.mancini@mnhn.lu>
2025-01-09 15:06:17 +0100

    Adde menu for games

--- exhibition/exhibition/cms_menus.py
+++ exhibition/exhibition/cms_menus.py
@@ -5,7 +5,7 @@ from cms.utils.urlutils import admin_reverse
 
 from django.utils.translation import gettext_lazy as _
 
-class ExhibitionToolbar(CMSToolbar):
+class ExhibitionCMSToolbar(CMSToolbar):
     def populate(self):
         menu = self.toolbar.get_or_create_menu(
             key='exhibitioncms_objects',
@@ -28,4 +28,33 @@ class ExhibitionToolbar(CMSToolbar):
         )
 
 
-toolbar_pool.register(ExhibitionToolbar)
+class GamesToolbar(CMSToolbar):
+    def populate(self):
+        menu = self.toolbar.get_or_create_menu(
+            key='django_jigsaw_puzzle',
+            verbose_name=_('museo.pro/games')
+        )
+
+        menu.add_sideframe_item(
+            name=_('Jigsaw puzzles'),
+            url=admin_reverse('django_jigsaw_puzzle_jigsawpuzzle_changelist')
+        )
+
+        menu.add_sideframe_item(
+            name=_('Memory games'),
+            url=admin_reverse('django_jigsaw_puzzle_memorygame_changelist')
+        )
+
+        menu.add_sideframe_item(
+            name=_('Difficulty levels'),
+            url=admin_reverse('django_jigsaw_puzzle_difficultylevel_changelist')
+        )
+
+        menu.add_sideframe_item(
+            name=_('Image sets'),
+            url=admin_reverse('django_jigsaw_puzzle_imageset_changelist')
+        )
+
+
+toolbar_pool.register(ExhibitionCMSToolbar)
+toolbar_pool.register(GamesToolbar)
