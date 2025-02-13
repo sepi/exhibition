fff52e64ec4c537b9a1f7be7d3485f8cc064215f
Raffael Mancini <raffael.mancini@mnhn.lu>
2025-02-13 07:58:24 +0100

    Experimental changes

--- exhibition/django_jigsaw_puzzle/js_src/museopaint.js
+++ exhibition/django_jigsaw_puzzle/js_src/museopaint.js
@@ -1,3 +1,25 @@
+function rgb2hsl(r,g,b) {
+  let v=Math.max(r,g,b), c=v-Math.min(r,g,b), f=(1-Math.abs(v+v-c-1)); 
+  let h= c && ((v==r) ? (g-b)/c : ((v==g) ? 2+(b-r)/c : 4+(r-g)/c)); 
+  return [60*(h<0?h+6:h), f ? c/f : 0, (v+v-c)/2];
+}
+
+function hsl2rgb(h,s,l) 
+{
+   let a=s*Math.min(l,1-l);
+   let f= (n,k=(n+h/30)%12) => l - a*Math.max(Math.min(k-3,9-k,1),-1);
+   return [f(0),f(8),f(4)];
+}
+
+// Adjust the hue by a certain degree
+function adjustHue(r, g, b, degrees) { // 
+    let [h, s, l] = rgb2hsl(r, g, b);
+    console.log(h, s, l);
+    h = (h + degrees) % 360; // Adjust hue and keep it in range [0, 360]
+    console.log(h, s, l);
+    return hsl2rgb(h, s, l);
+}
+
 function dist(a, b) {
     if (!a[0] || !a[1] || !b[0] || !b[1]) {
 	return 0;
@@ -7,30 +29,60 @@ function dist(a, b) {
     return Math.sqrt(dx*dx + dy*dy);
 }
 
-export default function museopaint() {
-    // DOM
-    const rootEl = document.getElementById("game");
-    rootEl.style.width = "100vw";
-    rootEl.style.height = "100vh";
+function drawDot(gl, p,
+		 pos, vertsLen,
+		 color, radius, opacity) {
+    const [x, y] = pos;
+    const [r, g, b] = color;
 
-    const canvas = document.createElement('canvas');
+    const colorLoc = gl.getUniformLocation(p, "color");
+    gl.uniform4f(colorLoc, r, g, b, opacity);
+    const dotRadiusLoc = gl.getUniformLocation(p, "dotRadius");
+    gl.uniform1f(dotRadiusLoc, radius);
+    const dotCenterLoc = gl.getUniformLocation(p, "dotCenter");
+    gl.uniform2f(dotCenterLoc, x, y);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertsLen / 3);
+}
 
-    canvas.width = rootEl.clientWidth;
-    canvas.height = rootEl.clientHeight;
-    rootEl.appendChild(canvas);
+function drawDotsOnLine(gl, p,
+			posLast, pos, vertsLen, minDist,
+			color, radius, opacity) {
+    const [xFrom, yFrom] = posLast;
+    const [xTo, yTo] = pos;
 
-    // Config
-    const dotThreshold = 2;
-    let dotRadius = 10;
-    let dotOpacity = 0.01;
-    let dotColor = [0, 20, 255];
+    const d = dist(posLast, pos);
+    const direction = [(xTo-xFrom)/d, (yTo-yFrom)/d];
+    let dPainted = 0;
+    let posNew = [xFrom, yFrom];
+    let posNewPrev = [null, null];
+    while (dPainted < d - minDist) {
+	posNew = [posNew[0] + direction[0] * minDist,
+		  posNew[1] + direction[1] * minDist];
+	dPainted += dist(posNew, posNewPrev);
+	drawDot(gl, p, posNew, vertsLen, color, radius, opacity);
+	posNewPrev = posNew;
+    }
+}
+
+// Vertex data for a circle
+function generateCircleVertices(numSegments) {
+    const verts = [];
+    // Center vertex (fully opaque)
+    verts.push(0.0, 0.0, 1.0);
+    for (let i = 0; i <= numSegments; i++) {
+        const angle = (i / numSegments) * Math.PI * 2;
+        verts.push(Math.cos(angle), Math.sin(angle), 0.0);
+    }
+    return verts;
+}
 
 
+function initGl(canvas, dotResolution, dotThreshold, dotRadius, dotOpacity, dotColor) {
     // The drawing
     const gl = canvas.getContext("webgl", {
-	alpha: true,
+	alpha: false,
 	premultipliedAlpha: false, // important for precision!!
-	preserveDrawingBuffer: true,
+	preserveDrawingBuffer: true, // So the buffer is not deleted on every blit
     });
 
     // Shaders
@@ -45,12 +97,12 @@ uniform float dotRadius;
 varying float vAlpha; // To be passed to frag shader
 
 void main() {
-  vec2 normalizedPos = ((pos*dotRadius + dotCenter) / resolution) * 2.0 - 1.0;
+  vec2 normalizedPos = ((pos * dotRadius + dotCenter) / resolution) * 2.0 - 1.0;
   normalizedPos.y *= -1.0;
   gl_Position = vec4(normalizedPos, 0, 1);
   vAlpha = alpha;
-}
-`;
+}`;
+
     const fs = `
 precision mediump float;
 uniform vec4 color;
@@ -70,22 +122,8 @@ void main() {
     gl.linkProgram(p);
     gl.useProgram(p);
 
-    // Vertex data for a circle
-    function generateCircleVertices(numSegments = 32) {
-	const verts = [];
-	// Center vertex (fully opaque)
-	verts.push(0.0, 0.0, 1.0);
-	for (let i = 0; i <= numSegments; i++) {
-            const angle = (i / numSegments) * Math.PI * 2;
-	    console.log(i, numSegments, angle);
-            verts.push(Math.cos(angle), Math.sin(angle), 0.0);
-	}
-	return verts;
-    }
-
     // Create a buffer for the circle's vertices
-    const verts = generateCircleVertices(128);
-    console.log(verts);
+    const verts = generateCircleVertices(dotResolution);
     const buf = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buf);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
@@ -106,7 +144,6 @@ void main() {
     const colorLoc = gl.getUniformLocation(p, "color");
     gl.uniform4f(colorLoc, dotColor[0], dotColor[1], dotColor[2], dotOpacity);
 
-
     // Set up the position attribute
     const posLoc = gl.getAttribLocation(p, "pos");
     gl.vertexAttribPointer(posLoc, 2, gl.FLOAT, false, 3 * 4, 0);
@@ -118,19 +155,26 @@ void main() {
     
     // Enable blending for alpha blending
     gl.enable(gl.BLEND);
-    gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA); // Standard alpha blending
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA); // Standard 
+
+    // gl.blendFuncSeparate(gl.ONE, gl.ONE_MINUS_SRC_ALPHA, gl.ONE_MINUS_DST_ALPHA, gl.ONE);
+    // gl.blendEquation(gl.FUNC_ADD);
+
+    // gl.blendFunc(gl.SRC_COLOR, gl.ONE_MINUS_SRC_COLOR);
+    // gl.blendFuncSeparate(gl.SRC_ALPHA, gl.ONE, gl.ONE, gl.ONE_MINUS_SRC_ALPHA)
+    // gl.blendEquation(gl.FUNC_ADD);
 
     // Set up WebGL viewport and clear color
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Click handling
-    let mouseDown = false;
-    let posLast = [null, null];
-    let drawnLast = [null, null];
-    let traceDist = 0;
+    return [gl, p, verts.length];
+}
 
+function initEventListeners(canvas, gl, p, vertsLen,
+			    mouseDown, posLast, drawnLast, traceDist,
+			    dotThreshold, dotRadius, dotColor, dotOpacity) {
     canvas.addEventListener('mousedown', (ev) => {
 	mouseDown = true;
 	posLast = [ev.clientX, ev.clientY];
@@ -140,8 +184,8 @@ void main() {
 	// Single clicks
 	if (!drawnLast[0]) {
 	    const pos = [ev.clientX, ev.clientY];
-	    for (let i = 0; i < 11; ++i) {
-		drawDot(pos, dotColor, dotRadius, dotOpacity);
+	    for (let i = 0; i < 1; ++i) {
+		drawDot(gl, p, pos, vertsLen, dotColor, dotRadius, dotOpacity);
 	    }
 	}
 
@@ -158,7 +202,9 @@ void main() {
 	    traceDist += d;
 
 	    if (traceDist >= dotThreshold) {
-		drawDotsOnLine(drawnLast, pos, dotThreshold,
+		drawDotsOnLine(gl, p,
+			       drawnLast, pos, vertsLen,
+			       dotThreshold,
 			       dotColor, dotRadius, dotOpacity);
 		traceDist = 0;
 		drawnLast = pos;
@@ -168,16 +214,14 @@ void main() {
     });
 
     canvas.addEventListener('wheel', (ev) => {
-	const shift = ev.getModifierState("Shift")
-
-	if (!shift) {
-	    dotRadius += -ev.deltaY/100;
-	    dotRadius = Math.round(dotRadius);
-	    if (dotRadius < 1) {
-		dotRadius = 1;
-	    }
-	    console.log("dotRadius", dotRadius);
-	} else {
+	const shift = ev.getModifierState("Shift");
+	const alt = ev.getModifierState("Alt");
+	if (alt && shift) {
+	    let [r, g, b] = dotColor;
+	    console.log(r, g, b);
+	    dotColor = adjustHue(r, g, b, ev.deltaY/10.0);
+	    console.log(dotColor);
+	} else if (shift) {
 	    dotOpacity += -ev.deltaY/100000.0
 	    if (dotOpacity < 0) {
 		dotOpacity = 0;
@@ -186,8 +230,14 @@ void main() {
 		dotOpacity = 1;
 	    }
 	    console.log("dotOpacity", dotOpacity);
+	} else {
+	    dotRadius += -ev.deltaY/100;
+	    dotRadius = Math.round(dotRadius);
+	    if (dotRadius < 1) {
+		dotRadius = 1;
+	    }
+	    console.log("dotRadius", dotRadius);
 	}
-	
     });
 
     canvas.addEventListener('mouseenter', (ev) => {
@@ -195,79 +245,47 @@ void main() {
 	    drawnLast = [ev.clientX, ev.clientY];
 	}
     });
+}
 
-    function drawDot(pos, color, radius, opacity) {
-	const [x, y] = pos;
-	const [r, g, b] = color;
+export default function museopaint() {
+    // DOM
+    const rootEl = document.getElementById("game");
+    rootEl.style.width = "100vw";
+    rootEl.style.height = "100vh";
 
-	gl.uniform4f(colorLoc, r, g, b, opacity); // Red with alpha 0.5
-	// gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.uniform1f(dotRadiusLoc, dotRadius);
-	gl.uniform2f(dotCenterLoc, x, y);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, verts.length / 3);
-	// const grd = gl.createRadialGradient(x, y, 0, x, y, radius, opacity);
-	// grd.addColorStop(0, `rgb(${r} ${g} ${b} / ${opacity/100}%)`);
-	// // grd.addColorStop(0.3, `rgba(${r} ${g} ${b} / 0.5%)`);
-	// grd.addColorStop(1, `rgb(${r} ${g} ${b} / 0%)`);
-	// gl.fillStyle = grd;
-	
-	// let c = `rgba(${r} ${g} ${b} / ${opacity/255.0}%)`;
-	// onscreenCtx.fillStyle = c;
-	// console.log(c);
-	// onscreenCtx.globalCompositeOperation = 'source-over';
-	// onscreenCtx.imageSmoothingEnabled = true;
-	// onscreenCtx.imageSmoothingQuality = 'high';
-	// onscreenCtx.globalAlpha = opacity/100;
-	
-	// gl.beginPath();
-	// gl.arc(x, y, radius, 0, 2*Math.PI, 0);
-	// gl.closePath();
-	// gl.fill();
+    const gizmosHtml = `
+<div style="position: absolute; top: 0, left: 0; width:200px; height: 100%; background: rgba(255, 255, 255, 0); pointer-events: none; z-index: 1;">
+  <span>Hello</span>
+  <input type="color" id="colorPicker" style="pointer-events: auto;">
+</div>
+`;
+    // rootEl.innerHtml = gizmosHtml;
+    // console.log(rootEl.innerHtml);
+    
+    const canvas = document.createElement('canvas');
+    // canvas.style = "position: absolute; top: 0, left: 0; z-index: 0;"
 
-	// const w = 2*radius - 1; // Always odd size
-	// const h = 2*radius - 1;
-	// const imageData = onscreenCtx.getImageData(x-radius, y-radius, w, h);
-	// for (let ix = 0; ix < w; ix++) {
-	//     for (let iy = 0; iy < h; iy++) {
-	// 	const i = (ix + iy * w) * 4;
-	// 	const d = dist([radius, radius], [ix+1, iy+1]);
-	// 	const alpha = (0.9*radius - d)/radius;
-	// 	console.log( imageData.data[i]);
-	// 	imageData.data[i] = Math.floor( (1.0-alpha) * imageData.data[i] + alpha * r);
-	// 	imageData.data[i + 1] = Math.floor( (1.0-alpha) * imageData.data[i + 1] + alpha * g);
-	// 	imageData.data[i + 2] = Math.floor( (1.0-alpha) * imageData.data[i + 2] + alpha * b);
-	// 	imageData.data[i + 3] = 255;
-	//     }
-	// }
-	// onscreenCtx.putImageData(imageData, x-radius, y-radius);
+    canvas.width = rootEl.clientWidth;
+    canvas.height = rootEl.clientHeight;
+    rootEl.appendChild(canvas);
 
+    // Config
+    const dotThreshold = 3;
+    let dotRadius = 20;
+    let dotOpacity = 0.6;
+    let dotColor = [1, 0, 0];
+    const dotResolution = 64;
 
-	
-	// Blit to on screen canvas
-	// offscreenCanvas.convertToBlob().then(blob => {
-	//     createImageBitmap(blob).then(bitmap => {
-	// 	onscreenCtx.drawImage(bitmap, 0, 0);
-	//     });
-	// });
-    }
+    const [gl, p, vertsLen] = initGl(canvas,
+				     dotResolution, dotThreshold, dotRadius, dotOpacity, dotColor);
 
-    function drawDotsOnLine(posLast, pos, minDist,
-			    color, radius, opacity) {
-	const [xFrom, yFrom] = posLast;
-	const [xTo, yTo] = pos;
+    // Click handling
+    let mouseDown = false;
+    let posLast = [null, null];
+    let drawnLast = [null, null];
+    let traceDist = 0;
 
-	const d = dist(posLast, pos);
-	const direction = [(xTo-xFrom)/d, (yTo-yFrom)/d];
-	let dPainted = 0;
-	let posNew = [xFrom, yFrom];
-	let posNewPrev = [null, null];
-	while (dPainted < d - minDist) {
-	    posNew = [posNew[0] + direction[0] * minDist,
-		      posNew[1] + direction[1] * minDist];
-	    dPainted += dist(posNew, posNewPrev);
-	    drawDot(posNew, color, radius, opacity);
-	    posNewPrev = posNew;
-	}
-	
-    }
+    initEventListeners(canvas, gl, p, vertsLen,
+		       mouseDown, posLast, drawnLast, traceDist,
+		       dotThreshold, dotRadius, dotColor, dotOpacity)
 }
